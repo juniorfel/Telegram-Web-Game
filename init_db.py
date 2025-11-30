152e7f9dc8a2f628772285dafe43795fa4fe37e5
JuniorFel <40219525+juniorfel@users.noreply.github.com>
2025-11-30 01:02:08 -0300

    Create init_db.py

---
+++ init_db.py
@@ -0,0 +1,6 @@
+from database import init_db
+
+if __name__ == "__main__":
+    print("Iniciando a criação das tabelas do banco de dados...")
+    init_db()
+    print("Criação das tabelas concluída.")
